e4218868c2cb6fda29e446b0bdbe88119e78cfe8
Daniel <danielbou@outlook.com.br>
2022-03-29 21:55:26 -0300

    ngDoCheck

--- meu-primeiro-app/src/app/app.component.ts
+++ meu-primeiro-app/src/app/app.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
   title = 'meu-primeiro-app';
+  public valor: number = 1;
 
-  ngOnInit(): void {
+  constructor() {}
 
+  public adicionar(): number {
+    return (this.valor += 1);
   }
 
+  ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
 }
