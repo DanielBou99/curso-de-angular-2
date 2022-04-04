01b98b525ab862f4d5f512c2cd7d4815225535ec
Daniel <danielbou@outlook.com.br>
2022-04-04 07:56:37 -0300

    Service - injeção de dependência

---
+++ meu-primeiro-app/src/app/shared/food-list/food-list.component.ts
@@ -0,0 +1,17 @@
+import { FoodListService } from './../../services/food-list.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-list',
+  templateUrl: './food-list.component.html',
+  styleUrls: ['./food-list.component.scss'],
+})
+export class FoodListComponent implements OnInit {
+  public foodList: Array<String> = [];
+
+  constructor(private foodListService: FoodListService) {}
+
+  ngOnInit(): void {
+    this.foodList = this.foodListService.foodList();
+  }
+}
