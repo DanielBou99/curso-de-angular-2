78075903222b939b5b16fd5210933ccb645f06c8
Daniel <danielbou@outlook.com.br>
2022-04-09 17:31:05 -0300

    Parametros nas rotas

--- routes/src/app/pages/sobre/sobre.component.ts
+++ routes/src/app/pages/sobre/sobre.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.scss']
+  styleUrls: ['./sobre.component.scss'],
 })
 export class SobreComponent implements OnInit {
-
-  constructor() { }
+  constructor(private activatedroute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activatedroute.params.subscribe((res) =>
+      console.log(res.id, res.username, res)
+    );
+    this.activatedroute.queryParams.subscribe((res) => console.log(res));
   }
-
 }
