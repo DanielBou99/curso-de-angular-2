c3dad34ea785b8c9ef5e25bf4c45d6661e589fdc
Daniel <danielbou@outlook.com.br>
2022-04-21 16:58:28 -0300

    CanActivate

--- guards/src/app/app-routing.module.ts
+++ guards/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './shared/account/account.component';
+import { CanActiveGuard } from './shared/guards/can-active.guard';
 import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'account', component: AccountComponent },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [CanActiveGuard],
+  },
 ];
 
 @NgModule({
